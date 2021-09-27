477b38b5ccbd00745b60335ba5b0c2176dd00e38
Mickael <40719970+Starius-Project@users.noreply.github.com>
2021-09-27 19:09:39 +0200

    lot of changmeents

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ radio.onReceivedString(function (receivedString) {
     if (next == 0) {
         if (receivedString == "1") {
             led.plotBarGraph(
-            Math.map(signal, -95, -42, 0, 9),
+            Math.map(signal, -128, -42, 0, 9),
             9
             )
-            if (Math.round(signal) >= 8) {
+            if (Math.round(Math.map(signal, -128, -42, 0, 9)) >= 8) {
                 basic.showString("SPECIMEN FOUND")
-                next = 1
                 basic.showIcon(IconNames.Yes)
                 basic.pause(1000)
+                next = 1
             }
         }
     } else if (next == 1) {
@@ -19,13 +19,16 @@ radio.onReceivedString(function (receivedString) {
             Math.map(signal, -95, -42, 0, 9),
             9
             )
-            if (Math.round(signal) >= 8) {
-                radio.sendString("UP")
+            if (Math.round(Math.map(signal, -128, -42, 0, 9)) >= 8) {
+                radio.sendNumber(5)
                 basic.showString("FUEL FOUND")
                 basic.showIcon(IconNames.Yes)
                 basic.pause(1000)
+                next = 3
             }
         }
+    } else if (next == 3) {
+        basic.showIcon(IconNames.Heart)
     }
 })
 let signal = 0
